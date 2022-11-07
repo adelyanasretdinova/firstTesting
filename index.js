f06709bc48c2b3562240317cc75fc92f74486e8a
adelyanasretdinova <adelya.nasretdinova@gmail.com>
2022-11-07 15:13:52 +0100

    simple arithmetical function testing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function addNumbers(a, b) {
   if (a > 0 && b > 0) {
     return a + b;
   } else {
-    return "parameter needs to be a number";
+    return "parameter needs to be a positive number";
   }
 }
 
--- index.test.js
+++ index.test.js
@@ -19,12 +19,24 @@ describe("a function to add 2 numbers", () => {
     //act: call function with 2 values
     let result = addNumbers("a", 1);
     // assert:check expectation
-    expect(result).toEqual("parameter needs to be a number");
+    expect(result).toEqual("parameter needs to be a positive number");
   });
-  it("doesnt add if first parameter is not a number", () => {
+  it("doesnt add if second parameter is not a number", () => {
     //act: call function with 2 values
     let result = addNumbers(1, "a");
     // assert:check expectation
-    expect(result).toEqual("parameter needs to be a number");
+    expect(result).toEqual("parameter needs to be a positive number");
+  });
+  it("doesnt add if first parameter is not a positive number", () => {
+    //act: call function with 2 values
+    let result = addNumbers(-1, 2);
+    // assert:check expectation
+    expect(result).toEqual("parameter needs to be a positive number");
+  });
+  it("doesnt add if second parameter is not a positive number", () => {
+    //act: call function with 2 values
+    let result = addNumbers(4, -2);
+    // assert:check expectation
+    expect(result).toEqual("parameter needs to be a positive number");
   });
 });
